b1980f4a534bb4c8688ab144c12dd3479e6ca690
Jasulan273 <54472410+Jasulan273@users.noreply.github.com>
2023-05-04 01:25:41 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Helmet } from 'react-helmet';
 import "../src/styles/style.css"
 import background from "./image/background.png"
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
@@ -10,6 +11,8 @@ import viber from "./image/viber.png"
 import card1 from "./image/card_1.png"
 import card2 from "./image/card_2.png"
 import card3 from "./image/card_3.png"
+import card4 from "./image/card_4.png"
+import card5 from "./image/card_5.png"
 import icon1 from "./image/icon_1.png"
 import icon2 from "./image/icon_2.png"
 import icon3 from "./image/icon_3.png"
@@ -24,87 +27,99 @@ import 'swiper/css/navigation';
 import { EffectCoverflow, Pagination, Navigation } from 'swiper';
 
 
-const API_KEY = "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";
+const API_KEY = "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";
 // "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
-  "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
+    "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
 }
 
 
 
 const App = () => {
+
+    function Submit(e) {
+        const formEle = document.querySelector("form");
+        const formDatab = new FormData(formEle);
+        fetch(
+            "https://script.google.com/macros/s/AKfycbwUdF3diLr0Cjf3cCwCbDXUINcYaqSNzOe12zb--4Q7RNFC7RXYxukGhrQSrALFtt16/exec",
+            {
+                method: "POST",
+                body: formDatab
+            }
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
     const [messages, setMessages] = useState([
         {
-          message: "Hello, I'm ChatGPT! Ask me anything!",
-          sentTime: "just now",
-          sender: "ChatGPT"
+            message: "Сәлем,мен осы платформаның жасанды интелектісі боламын!",
+            sentTime: "қазір",
+            sender: "ChatGPT"
         }
-      ]);
-      const [isTyping, setIsTyping] = useState(false);
-    
-      const handleSend = async (message) => {
+    ]);
+    const [isTyping, setIsTyping] = useState(false);
+
+    const handleSend = async (message) => {
         const newMessage = {
-          message,
-          direction: 'outgoing',
-          sender: "user"
+            message,
+            direction: 'outgoing',
+            sender: "user"
         };
-    
+
         const newMessages = [...messages, newMessage];
-        
+
         setMessages(newMessages);
-    
+
         // Initial system message to determine ChatGPT functionality
         // How it responds, how it talks, etc.
         setIsTyping(true);
         await processMessageToChatGPT(newMessages);
-      };
-    
-      async function processMessageToChatGPT(chatMessages) { // messages is an array of messages
-        // Format messages for chatGPT API
-        // API is expecting objects in format of { role: "user" or "assistant", "content": "message here"}
-        // So we need to reformat
-    
+    };
+
+    async function processMessageToChatGPT(chatMessages) { // messages is an array of messages
+
         let apiMessages = chatMessages.map((messageObject) => {
-          let role = "";
-          if (messageObject.sender === "ChatGPT") {
-            role = "assistant";
-          } else {
-            role = "user";
-          }
-          return { role: role, content: messageObject.message}
+            let role = "";
+            if (messageObject.sender === "ChatGPT") {
+                role = "assistant";
+            } else {
+                role = "user";
+            }
+            return { role: role, content: messageObject.message }
         });
-    
-    
-        // Get the request body set up with the model we plan to use
-        // and the messages which we formatted above. We add a system message in the front to'
-        // determine how we want chatGPT to act. 
+
         const apiRequestBody = {
-          "model": "gpt-3.5-turbo",
-          "messages": [
-            systemMessage,  // The system message DEFINES the logic of our chatGPT
-            ...apiMessages // The messages from our chat with ChatGPT
-          ]
+            "model": "gpt-3.5-turbo",
+            "messages": [
+                systemMessage,
+                ...apiMessages
+            ]
         }
-    
-        await fetch("https://api.openai.com/v1/chat/completions", 
-        {
-          method: "POST",
-          headers: {
-            "Authorization": "Bearer " + API_KEY,
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(apiRequestBody)
-        }).then((data) => {
-          return data.json();
-        }).then((data) => {
-          console.log(data);
-          setMessages([...chatMessages, {
-            message: data.choices[0].message.content,
-            sender: "ChatGPT"
-          }]);
-          setIsTyping(false);
-        });
-      }
+
+        await fetch("https://api.openai.com/v1/chat/completions",
+            {
+                method: "POST",
+                headers: {
+                    "Authorization": "Bearer " + API_KEY,
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(apiRequestBody)
+            }).then((data) => {
+                return data.json();
+            }).then((data) => {
+                console.log(data);
+                setMessages([...chatMessages, {
+                    message: data.choices[0].message.content,
+                    sender: "ChatGPT"
+                }]);
+                setIsTyping(false);
+            });
+    }
     const [formDisplay, setFormDisplay] = useState(false);
 
     const toggleForm = () => {
@@ -167,34 +182,67 @@ const App = () => {
                 <div className="container">
                     <h1 className="title">ТУРЛАР</h1>
                     <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide className="SwiperSlider" style={{ backgroundImage: `url(${card1})` }}></SwiperSlide>
-        <SwiperSlide className="SwiperSlider" style={{ backgroundImage: `url(${card2})` }}></SwiperSlide>
-        <SwiperSlide className="SwiperSlider" style={{ backgroundImage: `url(${card3})` }}></SwiperSlide>
-        <SwiperSlide className="SwiperSlider" style={{ backgroundImage: `url(${card1})` }}></SwiperSlide>
-        <SwiperSlide className="SwiperSlider" style={{ backgroundImage: `url(${card2})` }}></SwiperSlide>
-        <SwiperSlide className="SwiperSlider" style={{ backgroundImage: `url(${card3})` }}></SwiperSlide>
-        <SwiperSlide className="SwiperSlider" style={{ backgroundImage: `url(${card1})` }}></SwiperSlide>
-        <SwiperSlide className="SwiperSlider" style={{ backgroundImage: `url(${card2})` }}></SwiperSlide>
-        <SwiperSlide className="SwiperSlider" style={{ backgroundImage: `url(${card3})` }}></SwiperSlide>
-      </Swiper>
+                        slidesPerView={3}
+                        spaceBetween={30}
+                        pagination={{
+                            clickable: true,
+                        }}
+                        modules={[Pagination]}
+                        className="mySwiper"
+                        loop={true}
+                    >
+                        <SwiperSlide className="swiper-slider" style={{ backgroundImage: `url(${card1})`, display: "flex" }}>
+                            <h1 className="slider_card_text">Спорт тур</h1>
+                            <h2 className="slider_card_subtext">Велосипед, шаңғы, йога секілді табиғатпен байланысты спорт турлеріне тур</h2>
+                        </SwiperSlide>
+                        <SwiperSlide className="swiper-slider" style={{ backgroundImage: `url(${card2})`, display: "flex" }}>
+                            <h1 className="slider_card_text">Көрнекті жерлер</h1>
+                            <h2 className="slider_card_subtext">Әр қаланың тарихи және мәдени мұра ретінде қалдырылған жерлер</h2>
+                        </SwiperSlide>
+                        <SwiperSlide className="swiper-slider" style={{ backgroundImage: `url(${card3})`, display: "flex" }}>
+                            <h1 className="slider_card_text">Круиз/яхта</h1>
+                            <h2 className="slider_card_subtext">Ашық теңіз, мұхитта кемені көлік құралы ретінде пайдаланатын топтық экскурсия</h2>
+                        </SwiperSlide>
+                        <SwiperSlide className="swiper-slider" style={{ backgroundImage: `url(${card4})`, display: "flex" }}>
+                            <h1 className="slider_card_text">Жеке тур</h1>
+                            <h2 className="slider_card_subtext">Әр қаланы жеке, менеджермен байланыса отырып өз бақылауыңызда болады</h2>
+                        </SwiperSlide>
+                        <SwiperSlide className="swiper-slider" style={{ backgroundImage: `url(${card5})`, display: "flex" }}>
+                            <h1 className="slider_card_text">Виртуалды тур</h1>
+                            <h2 className="slider_card_subtext">Шынайы турмен танысып, шынайы эмоцияға бөленіңіз</h2>
+                        </SwiperSlide>
+                        <SwiperSlide className="swiper-slider" style={{ backgroundImage: `url(${card1})`, display: "flex" }}>
+                            <h1 className="slider_card_text">Көрнекті жерлер</h1>
+                            <h2 className="slider_card_subtext">Әр қаланың тарихи және мәдени мұра ретінде қалдырылған жерлер</h2>
+                        </SwiperSlide>
+                        <SwiperSlide className="swiper-slider" style={{ backgroundImage: `url(${card2})`, display: "flex" }}>
+                            <h1 className="slider_card_text">Көрнекті жерлер</h1>
+                            <h2 className="slider_card_subtext">Әр қаланың тарихи және мәдени мұра ретінде қалдырылған жерлер</h2>
+                        </SwiperSlide>
+                        <SwiperSlide className="swiper-slider" style={{ backgroundImage: `url(${card3})`, display: "flex" }}>
+                            <h1 className="slider_card_text">Көрнекті жерлер</h1>
+                            <h2 className="slider_card_subtext">Әр қаланың тарихи және мәдени мұра ретінде қалдырылған жерлер</h2>
+                        </SwiperSlide>
+                        <SwiperSlide className="swiper-slider" style={{ backgroundImage: `url(${card4})`, display: "flex" }}>
+                            <h1 className="slider_card_text">Көрнекті жерлер</h1>
+                            <h2 className="slider_card_subtext">Әр қаланың тарихи және мәдени мұра ретінде қалдырылған жерлер</h2>
+                        </SwiperSlide>
+                        <SwiperSlide className="swiper-slider" style={{ backgroundImage: `url(${card5})`, display: "flex" }}>
+                            <h1 className="slider_card_text">Көрнекті жерлер</h1>
+                            <h2 className="slider_card_subtext">Әр қаланың тарихи және мәдени мұра ретінде қалдырылған жерлер</h2>
+                        </SwiperSlide>
+
+                    </Swiper>
                 </div>
             </div>
             <div className="form_panel">
                 <div className="container">
-                    <div className="form">
-                        <h1 id="form_title" className="title">ӨТІНІШ ЖІБЕРУ</h1>
-                        <input type="text" placeholder="Толық аты" />
-                        <input type="tel" placeholder="Телефон нөмірі" name="phone_number" pattern="\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}" required />
-                        <a href="#"><h1>Жіберу</h1></a>
-                    </div>
+                    <h1 className="title">Сауалнама</h1>
+                    <form className="form" onSubmit={(e) => Submit(e)}>
+                        <input className="form_panel_input" placeholder="Your Name" name="Name" type="text" />
+                        <input className="form_panel_input" placeholder="Your Message" name="Number" type="text" />
+                        <input className="form_panel_button" value="Жіберу" name="Name" type="submit" />
+                    </form>
                 </div>
             </div>
 
@@ -222,22 +270,22 @@ const App = () => {
             ></button>
 
             <div className="chat-popup" id="myForm" style={{ display: formDisplay ? "block" : "none" }}>
-                   <div style={{ position:"relative", height: "800px", width: "700px"  }}>
-        <MainContainer>
-          <ChatContainer>       
-            <MessageList 
-              scrollBehavior="smooth" 
-              typingIndicator={isTyping ? <TypingIndicator content="ChatGPT is typing" /> : null}
-            >
-              {messages.map((message, i) => {
-                console.log(message)
-                return <Message key={i} model={message} />
-              })}
-            </MessageList>
-            <MessageInput placeholder="Type message here" onSend={handleSend} />        
-          </ChatContainer>
-        </MainContainer>
-      </div>
+                <div style={{ position: "relative", height: "600px", width: "500px", borderRadius: "15px" }}>
+                    <MainContainer>
+                        <ChatContainer >
+                            <MessageList
+                                scrollBehavior="smooth"
+                                typingIndicator={isTyping ? <TypingIndicator content="ChatGPT is typing" /> : null}
+                            >
+                                {messages.map((message, i) => {
+                                    console.log(message)
+                                    return <Message key={i} model={message} />
+                                })}
+                            </MessageList>
+                            <MessageInput placeholder="Type message here" onSend={handleSend} />
+                        </ChatContainer>=
+                    </MainContainer>
+                </div>
             </div>
         </div>
     )
